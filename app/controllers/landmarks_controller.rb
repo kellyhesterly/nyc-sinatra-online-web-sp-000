6f10517bfc6e5590cbccb49e39521ca59ffd63e4
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-05-24 18:10:24 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -10,6 +10,6 @@ class LandmarksController < ApplicationController
   end
 
   post '/landmarks' do
-    @landmark = Landmark.create(params)
+    @landmark = Landmark.create(params[:landmark])
   end
 end
